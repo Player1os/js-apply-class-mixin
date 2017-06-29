1c02f608b0bffb5af31d94332788e0dea476bbc9
Osama <osama.hassanein@cellense.com>
2017-06-29 21:33:01 +0200

    Implement the base library

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello') // tslint:disable-line no-console
+// Define and expose the constructor interface.
+export interface IConstructor {
+	new(...args:any[]): object,
+}
+
+// Expose the mixin function.
+export default (baseConstructors: IConstructor[]) => {
+	// Return a handler for the result constructor.
+	return (derivedConstructor: IConstructor) => {
+		// Iterate through each of the supplied base constructors.
+		baseConstructors.forEach((baseConstructor) => {
+			// Copy all own properties from the base constructor's prototype to the derived constructor's prototype.
+			Object.getOwnPropertyNames(baseConstructor.prototype).forEach((name) => {
+				derivedConstructor.prototype[name] = baseConstructor.prototype[name]
+			})
+		})
+	}
+}
