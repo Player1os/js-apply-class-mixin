b8f5899352a3694df5dcb8fe97319f5f5a8259c1
Osama Hassanein <player1os@gmail.com>
2017-06-30 10:09:05 +0200

    Refactor to only use a single base constructor

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,12 @@ export interface IConstructor {
 }
 
 // Expose the mixin function.
-export default (baseConstructors: IConstructor[]) => {
+export default (baseConstructor: IConstructor) => {
 	// Return a handler for the result constructor.
 	return (derivedConstructor: IConstructor) => {
-		// Iterate through each of the supplied base constructors.
-		baseConstructors.forEach((baseConstructor) => {
-			// Copy all own properties from the base constructor's prototype to the derived constructor's prototype.
-			Object.getOwnPropertyNames(baseConstructor.prototype).forEach((name) => {
-				derivedConstructor.prototype[name] = baseConstructor.prototype[name]
-			})
+		// Copy all own properties from the base constructor's prototype to the derived constructor's prototype.
+		Object.getOwnPropertyNames(baseConstructor.prototype).forEach((name) => {
+			derivedConstructor.prototype[name] = baseConstructor.prototype[name]
 		})
 	}
 }
